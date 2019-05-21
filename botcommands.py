621b36353c8af8fbc65d07a87d553e54f447e065
Alex <sasha061200@gmail.com>
2019-05-22 00:24:39 +0300

    Add files via upload

---
+++ botcommands.py
@@ -0,0 +1,12 @@
+aname = 'admin'
+apass = 'pass'
+
+chaname = 'Admin username was changed to '
+chapass = 'Admin password was changed to '
+
+help = """/login - login, but u can do it, if know necessary info"""
+adminhelp = """/ahelp - help only for admin
+/chadmname - change admin username
+/chadmpass - change admin password
+/logout - logout
+"""
---
+++ const.py
@@ -0,0 +1,1 @@
+token = '850151607:AAEtWZhrN0_TjL70KQhYPB3dldP0bxH-2lM'
